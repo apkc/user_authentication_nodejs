04ea441230db5f671c5c009ed88efa70bb128846
apkc <avishkarpratapkc@gmail.com>
2017-03-11 19:21:28 +0545

    Upto bcrypt

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 mongoose.connect('mongodb://localhost/nodeauth');
 var db = mongoose.connection;
 
@@ -11,7 +12,7 @@ var UserSchema = mongoose.Schema({
     index: true
   },
   password: {
-    type: String
+    type: String, required: true, bcrypt: true
   },
   email: {
     type: String
@@ -27,6 +28,12 @@ var UserSchema = mongoose.Schema({
 var User = module.exports = mongoose.model('User', UserSchema);
 
 module.exports.createUser = function(newUser, callback){
-  newUser.save(callback);
+  bcrypt.hash(newUser.password, 10, function(err, hash){
+    if(err) throw err;
+    //Set hashed password
+    newUser.password = hash;
+    //Create User
+    newUser.save(callback);
+  });
   
 }
